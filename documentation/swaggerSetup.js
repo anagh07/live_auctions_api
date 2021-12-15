e02c987439fda8882a90c1197ed495bd4019cac3
Anagh Mehran <anagh07@gmail.com>
2021-12-15 14:00:29 -0500

    API Documentation

---
+++ documentation/swaggerSetup.js
@@ -0,0 +1,26 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+
+const apiBaseUrl = process.env.API_BASE_URL || 'http://localhost:5000';
+const options = {
+  apis: ['./documentation/doc.yaml'],
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Live Auctions API',
+      version: '1.0.0',
+      desciption: 'REST api for conducting live auctions.',
+      host: apiBaseUrl,
+      basePath: '/',
+      consumes: 'application/json',
+      produces: 'application/json',
+    },
+  },
+  server: [
+    {
+      url: apiBaseUrl,
+    },
+  ],
+};
+
+const spec = swaggerJsDoc(options);
+module.exports = spec;
